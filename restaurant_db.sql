d781e1c0f098e6b7f57136e3fc97aaafbb47278b
Carolyn Lam <caro_lyn_@hotmail.com>
2016-10-05 00:09:20 -0400

    add comments describing code

--- restaurant_db.sql
+++ restaurant_db.sql
@@ -21,19 +21,35 @@ insert into restaurant values
   (default, 'Liquid Art House', 355, 5, 'American', 'Faroe Island Salmon', FALSE, '2014-09-14'),
   (default, 'The Rattlesnake Bar & Grill', 351, 2, 'American', 'Spinach Dip', FALSE, '2015-07-19');
 
+-- to connect to db in terminal type '\connect db_name_goes_here'
 
 -- WHEN YOU NEED TO DELETE THE TABLE
 -- WRITE "drop table NAME_OF_TABLE"
 -- THEN CONTINUE AS BEFORE
 
 -- The names of the restaurants that you gave a 5 stars to
-
+-- select name from restaurant where stars = 5;
 
 -- The favorite dishes of all 5-star restaurants
+-- select favorite_dish from restaurant;
+
 -- The the id of a restaurant by a specific restaurant name, say 'NaanStop'
--- restaurants in the category of 'BBQ'
+-- select id from restaurant where name = 'Eastern Standard';
+
+-- restaurants in the category of 'American'
+-- select name from restaurant where category = 'American';
+
 -- restaurants that do take out
--- restaurants that do take out and is in the category of 'BBQ'
--- restaurants within 2 miles
--- restaurants you haven't ate at in the last week
--- restaurants you haven't ate at in the last week and has 5 stars
+-- select name from restaurant where does_takeout = TRUE;
+
+-- restaurants that do take out and is in the category of 'American'
+-- select name from restaurant where does_takeout = TRUE and category = 'American';
+
+-- restaurants within 400 miles
+--  select name from restaurant where distance <= 400;
+
+-- restaurants you haven't eaten at this year:
+-- select name from restaurant where last_time_you_ate_there < '2016-01-01';
+
+-- restaurants you haven't eaten at this year and has 5 stars
+-- select name from restaurant where last_time_you_ate_there < '2016-01-01' and stars = 5;
